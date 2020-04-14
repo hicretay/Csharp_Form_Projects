b475ac30d24d40c0f13dc1e52be49e04ebe95900
Hicret <hiicretay@hotmail.com>
2020-04-14 14:20:28 +0300

    14.04.2020

---
+++ Access_Kayit_Ekleme/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Access_Kayit_Ekleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Provider=Microsoft.Jet.OLEDB.4.0;Data Source = "C:\Users\hiicr\Desktop\Youtube access\Database1.mdb"
+        OleDbConnection baglan = new OleDbConnection("Provider = Microsoft.Jet.OLEDB.4.0; Data Source = C:\\Users\\hiicr\\Desktop\\Youtube access\\Database1.mdb");
+        OleDbCommand komut = new OleDbCommand();
+        private void verilerigoruntule()
+        {
+            listView1.Items.Clear();
+            baglan.Open();
+            OleDbCommand komut = new OleDbCommand();
+            komut.Connection = baglan;
+            komut.CommandText = ("Select *From Bilgiler");
+            OleDbDataReader oku = komut.ExecuteReader();
+
+            while (oku.Read())
+            {
+                ListViewItem ekle = new ListViewItem();
+                ekle.Text = oku["id"].ToString();
+                ekle.SubItems.Add(oku["Ad"].ToString());
+                ekle.SubItems.Add(oku["Soyad"].ToString());
+                ekle.SubItems.Add(oku["Yaş"].ToString());
+                ekle.SubItems.Add(oku["İl"].ToString());
+
+                listView1.Items.Add(ekle);
+            }
+            baglan.Close();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            verilerigoruntule();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            baglan.Open();
+            OleDbCommand komut = new OleDbCommand("insert into Bilgiler (id,Ad,Soyad,Yaş,İl) values('" 
+            + textBox5.Text.ToString()+"','"+textBox1.Text.ToString() + "','" + textBox2.Text.ToString() + "','" + textBox3.Text.ToString() + "','" + textBox4.Text.ToString() + "')", baglan);
+            komut.ExecuteNonQuery();
+            baglan.Close();
+            verilerigoruntule();
+                                                
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            baglan.Open();
+            komut.Connection = baglan;
+            komut.CommandText = "delete from Bilgiler where id='" + textBox6.Text + "'";
+            komut.ExecuteNonQuery();
+            baglan.Close();
+            verilerigoruntule();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            baglan.Open();
+            komut.Connection = baglan;
+            komut.CommandText = "update Bilgiler set Ad='" + textBox1.Text + "',Soyad='" + textBox2.Text + "',Yaş='" + textBox3.Text + "',İl='" + textBox4.Text + "'where id='" + textBox5.Text + "'";
+            komut.ExecuteNonQuery();
+            baglan.Close();
+            verilerigoruntule();
+        }
+    }
+}
