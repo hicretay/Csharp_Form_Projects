6adb0a62118b439bc2fbdfa686dbe8a509e15add
Hicret <hiicretay@hotmail.com>
2020-04-19 20:50:55 +0300

    19.04.2020

---
+++ Asansor_Uygulamasi/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Asansor_Uygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.buttonyukari = new System.Windows.Forms.Button();
+            this.buttonasagı = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(63, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 338);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(275, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 338);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(63, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(295, 32);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(63, 428);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(295, 32);
+            this.label4.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(179, 351);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 76);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Sienna;
+            this.label5.Location = new System.Drawing.Point(275, 374);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 51);
+            this.label5.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Sienna;
+            this.label6.Location = new System.Drawing.Point(63, 377);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 51);
+            this.label6.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Sienna;
+            this.label7.Location = new System.Drawing.Point(63, 169);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 51);
+            this.label7.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Sienna;
+            this.label8.Location = new System.Drawing.Point(275, 169);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(83, 51);
+            this.label8.TabIndex = 8;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(60, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(302, 84);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // buttonyukari
+            // 
+            this.buttonyukari.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonyukari.BackgroundImage")));
+            this.buttonyukari.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonyukari.Location = new System.Drawing.Point(385, 340);
+            this.buttonyukari.Name = "buttonyukari";
+            this.buttonyukari.Size = new System.Drawing.Size(56, 81);
+            this.buttonyukari.TabIndex = 10;
+            this.buttonyukari.UseVisualStyleBackColor = true;
+            this.buttonyukari.Click += new System.EventHandler(this.buttonyukari_Click);
+            // 
+            // buttonasagı
+            // 
+            this.buttonasagı.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonasagı.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonasagı.BackgroundImage")));
+            this.buttonasagı.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonasagı.Location = new System.Drawing.Point(460, 340);
+            this.buttonasagı.Name = "buttonasagı";
+            this.buttonasagı.Size = new System.Drawing.Size(56, 81);
+            this.buttonasagı.TabIndex = 11;
+            this.buttonasagı.UseVisualStyleBackColor = false;
+            this.buttonasagı.Click += new System.EventHandler(this.buttonasagı_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(447, 310);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "0";
+            this.label9.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(534, 431);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.buttonasagı);
+            this.Controls.Add(this.buttonyukari);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Asansör Uygulaması";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button buttonyukari;
+        private System.Windows.Forms.Button buttonasagı;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Asansor_Uygulamasi/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asansor_Uygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonyukari_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            label9.Text = "1";
+            label5.BackColor = Color.Sienna;
+            label6.BackColor = Color.Sienna;
+
+        }
+
+        private void buttonasagı_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            label9.Text = "0";
+            label7.BackColor = Color.Sienna;
+            label8.BackColor = Color.Sienna;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (label9.Text == "1")
+            {
+                pictureBox1.Top -= 5;
+                if(pictureBox1.Location.Y<=169)
+                {
+                    timer1.Stop();
+                    label7.BackColor = Color.Yellow;
+                    label8.BackColor = Color.Yellow;
+                }
+            }
+
+
+
+            if (label9.Text == "0")
+            { 
+                pictureBox1.Top += 5;
+                if(pictureBox1.Location.Y>=351)
+                {
+                    timer1.Stop();
+                    label5.BackColor = Color.Yellow;
+                    label6.BackColor = Color.Yellow;
+                }
+            }
+        }
+    }
+}
