e3b24c6d77609240b6bb930415c4f79dfa2d6c71
Hicret <hiicretay@hotmail.com>
2020-04-22 16:23:37 +0300

    22.04.2020

---
+++ Metin_Belgesi_Uygulamasi/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Metin_Belgesi_Uygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_url = new System.Windows.Forms.Button();
+            this.button_kaydet = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button_listele = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button_txtgoster = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_veri_kaydet = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(14, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Belge Adı:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(92, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(92, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(211, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(14, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Belge Yolu:";
+            // 
+            // button_url
+            // 
+            this.button_url.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button_url.Location = new System.Drawing.Point(92, 113);
+            this.button_url.Name = "button_url";
+            this.button_url.Size = new System.Drawing.Size(74, 23);
+            this.button_url.TabIndex = 4;
+            this.button_url.Text = "URL";
+            this.button_url.UseVisualStyleBackColor = true;
+            this.button_url.Click += new System.EventHandler(this.button_url_Click);
+            // 
+            // button_kaydet
+            // 
+            this.button_kaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button_kaydet.Location = new System.Drawing.Point(180, 113);
+            this.button_kaydet.Name = "button_kaydet";
+            this.button_kaydet.Size = new System.Drawing.Size(74, 23);
+            this.button_kaydet.TabIndex = 5;
+            this.button_kaydet.Text = "Kaydet";
+            this.button_kaydet.UseVisualStyleBackColor = true;
+            this.button_kaydet.Click += new System.EventHandler(this.button_kaydet_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button_listele
+            // 
+            this.button_listele.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button_listele.Location = new System.Drawing.Point(331, 15);
+            this.button_listele.Name = "button_listele";
+            this.button_listele.Size = new System.Drawing.Size(74, 23);
+            this.button_listele.TabIndex = 6;
+            this.button_listele.Text = "Listele";
+            this.button_listele.UseVisualStyleBackColor = true;
+            this.button_listele.Click += new System.EventHandler(this.button_listele_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(5, 162);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(150, 199);
+            this.listBox1.TabIndex = 7;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(161, 163);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(130, 199);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(177, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Textbox\'ta göster";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(60, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Listele";
+            // 
+            // button_txtgoster
+            // 
+            this.button_txtgoster.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button_txtgoster.Location = new System.Drawing.Point(331, 44);
+            this.button_txtgoster.Name = "button_txtgoster";
+            this.button_txtgoster.Size = new System.Drawing.Size(74, 23);
+            this.button_txtgoster.TabIndex = 11;
+            this.button_txtgoster.Text = "txt goster";
+            this.button_txtgoster.UseVisualStyleBackColor = true;
+            this.button_txtgoster.Click += new System.EventHandler(this.button_txtgoster_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox3.Location = new System.Drawing.Point(92, 78);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(211, 23);
+            this.textBox3.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(25, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "txt metin:";
+            // 
+            // btn_veri_kaydet
+            // 
+            this.btn_veri_kaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_veri_kaydet.Location = new System.Drawing.Point(331, 73);
+            this.btn_veri_kaydet.Name = "btn_veri_kaydet";
+            this.btn_veri_kaydet.Size = new System.Drawing.Size(74, 40);
+            this.btn_veri_kaydet.TabIndex = 14;
+            this.btn_veri_kaydet.Text = "verileri kaydet";
+            this.btn_veri_kaydet.UseVisualStyleBackColor = true;
+            this.btn_veri_kaydet.Click += new System.EventHandler(this.btn_veri_kaydet_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(297, 163);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(130, 199);
+            this.richTextBox2.TabIndex = 15;
+            this.richTextBox2.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(328, 143);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Verileri Yaz";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(435, 369);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.btn_veri_kaydet);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button_txtgoster);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button_listele);
+            this.Controls.Add(this.button_kaydet);
+            this.Controls.Add(this.button_url);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Metin Belgesi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_url;
+        private System.Windows.Forms.Button button_kaydet;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button_listele;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button_txtgoster;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_veri_kaydet;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Metin_Belgesi_Uygulamasi/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metin_Belgesi_Uygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        StreamWriter sw; 
+        string belgeadi, belgeyolu;
+
+        private void button_url_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)  //dosya acıldıgında yeni pencere acmak icin
+            {
+                belgeyolu = folderBrowserDialog1.SelectedPath.ToString();
+                textBox2.Text = belgeyolu.ToString();
+
+            }
+        }
+
+        private void button_kaydet_Click(object sender, EventArgs e)
+        {
+            belgeadi = textBox1.Text;
+            sw = File.CreateText(belgeyolu + "\\" + belgeadi + ".txt");
+            sw.Close(); 
+        }
+
+        private void button_listele_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader oku = new StreamReader(openFileDialog1.FileName);
+                string satir = oku.ReadLine();
+
+                while (satir!=null)
+                {
+                    listBox1.Items.Add(satir);
+                    satir=oku.ReadLine();
+                }
+            }
+        }
+
+        private void btn_veri_kaydet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                saveFileDialog1.Title = "Kayıt yerini seçin";  //pencere baslıgı
+                saveFileDialog1.Filter = "Metin Dosyası(*.txt)|*.txt";  //metin dosyası olarak filtrelendi
+                saveFileDialog1.FilterIndex = 2;      
+                saveFileDialog1.InitialDirectory = "C:\\"; //ilk nerenin açılacagını seçer
+                saveFileDialog1.ShowDialog();
+                StreamWriter kaydet = new StreamWriter(saveFileDialog1.FileName);
+                kaydet.WriteLine(richTextBox2.Text);
+                kaydet.Close();
+                MessageBox.Show("kaynak metin belgesine yazdırıldı");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Hata oluştu işlem gerçekleştirilemedi!");
+            }
+        }
+
+        private void button_txtgoster_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ac = new OpenFileDialog();
+            ac.Filter = "Metin Dosyası(*.txt)|*.txt";
+            ac.Multiselect = false;
+
+            if (ac.ShowDialog()==System.Windows.Forms.DialogResult.OK)
+            {
+                textBox3.Text = ac.SafeFileName;
+                try
+                {
+                    StreamReader oku = new StreamReader(ac.FileName);
+                    richTextBox1.Text = oku.ReadToEnd();
+                    oku.Close();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Dosya okunurken hata oluştu");
+                }
+            }
+        }
+    }
+}
